0e0bff2e732a01e7d98a2a6a0c9f9825ab613871
DH <daniel@vendettistudios.com>
2022-07-29 02:41:09 -0700

    created function for revealing the first question

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,29 @@
 const startScreen = document.getElementById("startScreen");
 const startQuiz = document.getElementById("startButton");
+const choices = document.getElementById("choices")
+const question = document.getElementById("question")
+// const lastQuestion = questions.length - 1;
+
 let currentQuestionIndex = 0;
-// const questions = document.getElementById("questions");
 
 // Hides Start Screen, Starts Quiz,
 startQuiz.onclick = function () {
-  if (startScreen.style.display !== "none") {
-    startScreen.style.display = "none";
-  } else {
-    startScreen.style.display = "block";
-  }
+    if (startScreen.style.display !== "none") {
+        startScreen.style.display = "none";
+    } else {
+        startScreen.style.display = "block";
+    }
+    //  timer = setInterval(clockTick, 1000);
+    //  timerEl.textContent = sec;
+    revealQuestion();
+};
+// Start Timer
 
- 
 
+
+
+// this function will display the questions
+function revealQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    question.innerHTML = currentQuestion.title;
 };
