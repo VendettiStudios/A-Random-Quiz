d8e706ddff28109aed5e24b53778054ae7e9a0f0
DH <daniel@vendettistudios.com>
2022-07-29 01:06:49 -0700

    created hide function for start screen

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,28 +1,16 @@
-const quiz = document.getElementsById('quiz'); 
-const results = document.getElementById('results');
-const startBtn = document.getElementById('startBtn');
-const timer = document.getElementById('timer');
-const seconds = document.getElementById('seconds')
+const startScreen = document.getElementById("startScreen");
+const startQuiz = document.getElementById("startButton");
+let currentQuestionIndex = 0;
+// const questions = document.getElementById("questions");
 
-function clockTick() {
-    // time updates
-    sec--;
-    timerEl.textContent = sec;
-    // checks time remaining
-    if (sec <= 0) {
-        quizEnd();
-    }
-}
+// Hides Start Screen, Starts Quiz,
+startQuiz.onclick = function () {
+  if (startScreen.style.display !== "none") {
+    startScreen.style.display = "none";
+  } else {
+    startScreen.style.display = "block";
+  }
 
-// Start Page
+ 
 
-// Click Start button and first 
-// question displays and 75 second 
-// timer begins counting down, 
-
-// When answer is chosen, displays
-// whether correct or incorrect. 
-
-// If answer is incorrect, - 15 seconds/score
-
-// when quiz is finished, time left = score
+};
