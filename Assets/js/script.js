6a48f5a6390595366711de146d5f7011eba8602e
DH <daniel@vendettistudios.com>
2022-08-06 17:45:58 -0700

    added timer Display and countdown function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,32 +2,51 @@ const startScreen = document.getElementById("startScreen");
 const startQuiz = document.getElementById("startButton");
 const choices = document.getElementById("choices");
 const question = document.getElementById("question");
+const timer = document.getElementById("seconds");
 // const lastQuestion = questions.length - 1
-
+let showTimer;
+let seconds = 75;
 let currentQuestionIndex = 0;
 
-// Hides Start Screen, Starts Quiz,
+// Hides Start Screen, Starts Quiz, shows and starts timer
 startQuiz.onclick = function () {
-    if (startScreen.style.display !== "none") {
-        startScreen.style.display = "none";
-    } else {
-        startScreen.style.display = "block";
+  if (startScreen.style.display !== "none") {
+    startScreen.style.display = "none";
+  } else {
+    startScreen.style.display = "block";
+  }
+//  reveal timer - start countdown
+  showTimer = setInterval(startCountdown, 1000);
+  timer.textContent = seconds;
+  revealQuestion();
+//   start countdown function
+  function startCountdown() {
+    seconds--;
+    timer.textContent = seconds;
+
+    if (seconds <= 0) {
+      endQuiz();
     }
-    revealQuestion();
-    // revealChoices();
+  }
+  // renders first question and choices in array
+  function revealQuestion() {
+    const currentQuestion = questions[currentQuestionIndex].title;
+    const currentChoices = questions[currentQuestionIndex].choices;
+    question.innerHTML = currentQuestion;
+    choices.innerHTML = currentChoices;
+//  append button to each choice
+    
+  }
 };
+
+// function startCountdown{
+
+// }
 
 // incrementer increment value by one
 // this function will display the questions
 // const currentQuestion = currentQuestionIndex[0];
 //     let currentChoices = currentQuestionIndex[0];
-function revealQuestion() {
-    const currentQuestion = questions[0];
-    let currentChoices = questions[0];
-    question.innerHTML = currentQuestion.title;
-    choices.innerHTML = currentChoices.choices;
-};
-
 
 // Need timer/score countdown to reveal when click start
 // if statements
@@ -36,28 +55,26 @@ function revealQuestion() {
 // }
 // Need answers to reveal with buttons
 
-
 // function revealChoices(){
-    // let i = "choices"
-    // let currentChoices = questions[0];
-    
+// let i = "choices"
+// let currentChoices = questions[0];
+
 //     console.log(currentChoices.title);
 // }
 
 // revealChoices();
 
-// Need answers when clicked to display wrong or right 
-// then advance to next question, subtracting 15 seconds 
+// Need answers when clicked to display wrong or right
+// then advance to next question, subtracting 15 seconds
 // from the countdown if incorrect
 // function choiceClick(){
 
 // }
-// need final screen that triggers when array is finished, 
+// need final screen that triggers when array is finished,
 // with final score/time still displayed
 // function quizEnd(){
 
 // }
-
 
 // need to store and parse score/time and alias to scores page upon click submit
 
