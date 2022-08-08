623d8761c8e02a38960d42acf39c407b07a64781
DH <daniel@vendettistudios.com>
2022-08-08 13:11:10 -0700

    added javascript for scores page, and linked page, to buttons, etc

--- Assets/js/scores.js
+++ Assets/js/scores.js
@@ -1,7 +1,8 @@
+const highscoreList = document.getElementById("highscoresList");
+const clearScores = document.getElementById("clearScores");
 window.onload = function () {
   const currentHighscores =
     JSON.parse(window.localStorage.getItem("highscores")) || [];
-  const highscoreList = document.getElementById("highscoresList");
   for (let i = 0; i < currentHighscores.length; i++) {
     const highscore = document.createElement("li");
     highscore.innerHTML = `
@@ -12,6 +13,12 @@ window.onload = function () {
                 ${currentHighscores[i].score}
             </span>
         `;
-    highscoreList.appendChild();
+    highscoreList.appendChild(highscore);
   }
 };
+
+
+clearScores.onclick=function(){
+    window.localStorage.removeItem("highscores");
+    highscoreList.innerHTML = "";
+}
